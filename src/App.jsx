495422fb07bd95c49e53cf2eff58db8df9b394ce
Arshak <arshak0066@gmail.com>
2021-10-16 19:50:36 +0400

    added

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+import './App.css';
+
+function App() {
+  const [result,
+    setResult] = useState('')
+  const inClick = (e) => {
+    if (e.target.name) 
+      setResult(result.concat(e.target.name))
+  }
+  const clear = () => {
+    setResult('')
+  }
+  const backSpace = () => {
+    setResult(result.slice(0, -1))
+  }
+  const calculate = () => {
+    try {
+      setResult(eval(result))
+    } catch {setResult('Error')}
+  }
+
+  return (
+    <div className="App">
+      <form onSumbit={() => console.log('hi')}>
+        <input type='text' onChange={() => {}} value={result}/>
+        <div className="keypad">
+        <button type="button" onClick={clear} id='clear'>Clear</button>
+        <button type="button" onClick={backSpace}>C</button>
+        <button type="button" name='/' onClick={inClick}>/</button>
+        <button type="button" name='7' onClick={inClick}>7</button>
+        <button type="button" name='8' onClick={inClick}>8</button>
+        <button type="button" name='9' onClick={inClick}>9</button>
+        <button type="button" name='*' onClick={inClick}>*</button>
+        <button type="button" name='4' onClick={inClick}>4</button>
+        <button type="button" name='5' onClick={inClick}>5</button>
+        <button type="button" name='6' onClick={inClick}>6</button>
+        <button type="button" name='+' onClick={inClick}>+</button>
+        <button type="button" name='1' onClick={inClick}>1</button>
+        <button type="button" name='2' onClick={inClick}>2</button>
+        <button type="button" name='3' onClick={inClick}>3</button>
+        <button type="button" name='-' onClick={inClick}>-</button>
+        <button type="button" name='0' onClick={inClick}>0</button>
+        <button type="button" name='.' onClick={inClick}>.</button>
+        <button type="submit" name='=' id='result' onClick={calculate}>=</button>
+      </div>
+      </form>
+     
+    </div>
+
+  );
+}
+
+export default App;
